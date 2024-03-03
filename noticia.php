57df77359ef51f651aaea73e9dc62773887b3ed6
Luiz Zanotta <luizplzanotta@gmail.com>
2024-03-03 16:38:13 -0300

    first commit

---
+++ noticia.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET" action="noticia.php">
+        Nome: <input type="text" name="nome"> <br><br>
+        Idade: <input type="text" name="idade"><br><br>
+        E-mail: <input type="text" name="email"><br><br>
+        <button type="submit">Enviar</button>
+
+    </form>
+</body>
+</html>
